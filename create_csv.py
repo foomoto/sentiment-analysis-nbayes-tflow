dd906ef74b09f06bea6f5eb507de83bb0bb68f55
Folarin Omotoriogun <folarinomotoriogun@gmail.com>
2018-07-18 20:00:32 +0100

    fix csv

--- create_csv.py
+++ create_csv.py
@@ -3,6 +3,8 @@ with open("train_data.csv", "a") as csv:
     csv.write("sentiment, sentence\n")
     with open("train_data.txt") as f:
         for line in f:
-            csv_line = line.replace(' ', ',', 1)
-            csv.write(csv_line)
+            line = line.replace('\n', '')
+            sentiment = line.split(" ", 1)[0]
+            text = '"' + line.split(" ", 1)[1] + '"'
+            csv.write(sentiment + ',' + text + '\n')
 
